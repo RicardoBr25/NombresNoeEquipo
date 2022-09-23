e68fa7d1621b0898c2f38753f674ca4ee37a2609
YulisaKau <113855888+YulisaKau@users.noreply.github.com>
2022-09-23 14:29:59 -0500

    agregue mi nombre al main(mi nombre es yulisa abigail kau)

--- src/nombresejemplo/NombresEjemplo.java
+++ src/nombresejemplo/NombresEjemplo.java
@@ -21,6 +21,7 @@ public class NombresEjemplo {
         System.out.println("Mi nombre es Sinai");
         System.out.println("Mi nombre es Monse"); 
         System.out.println("Mi nombre es Citly");
+        System.out.println("mi nombre es yulisa");
     }
     
 }
